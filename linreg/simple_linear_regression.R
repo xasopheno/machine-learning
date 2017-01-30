a472d716f57a8fb257bd49722408a908b291d218
Danny Meyer <danny.meyer@gmail.com>
2017-01-30 18:01:19 -0500

    Adds finished simple Linear Regression

---
+++ linreg/simple_linear_regression.R
@@ -0,0 +1,48 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+# dataset = dataset[, 2:3]    
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience, 
+               data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualising theh Test set results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary), 
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+                color = 'blue') +
+  ggtitle('Salary vs Experience (training set') +
+  xlab('Years Experience') +
+  ylab('Salary')
+
+# Visualising theh Test set results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary), 
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            color = 'blue') +
+  ggtitle('Salary vs Experience (test set') +
+  xlab('Years Experience') +
+  ylab('Salary')
